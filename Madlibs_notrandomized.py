a91310b7bae01b864093bf22a3bab031c9b7fb24
j-kilzer <96318402+j-kilzer@users.noreply.github.com>
2021-12-28 14:01:12 -0500

    Update Madlibs_notrandomized.py

--- Madlibs_notrandomized.py
+++ Madlibs_notrandomized.py
@@ -1,18 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Madlibs utilizing user input for word choices
 import random
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-random
-
-# Press the green button in the gutter to run the script.
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 adj1 = input("Adjective: ")
 adj2 = input("Adjective: ")
 noun1 = input("Noun: ")
